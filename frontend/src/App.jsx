410df0ab2cb73471c8538f89c408648e4af55c7c
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-10-26 19:22:34 +0900

    fix :: Home import 추가

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import PlaceOrder from './pages/PlaceOrder/PlaceOrder'
 
---
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './ExploreMenu.css'
+
+const ExploreMenu = () => {
+  return (
+    <div>ExploreMenu</div>
+  )
+}
+
+export default ExploreMenu
