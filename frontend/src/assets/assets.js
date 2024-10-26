59db840c2a42b7f33902332fc28f9125d1903c88
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-10-26 23:57:25 +0900

    feat :: 백그라운드 이미지 삽입

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,11 +1,9 @@
 import beadal_icon from './beadal_icon.png'
 import search_icon from './search_icon.png'
 import bag_icon from './bag_icon.png'
-import Header from './Header.png'
 
 export const assets = {
     beadal_icon,
     search_icon,
     bag_icon,
-    Header
 }
