016157672584bc498d084c2bd6262d930a454b6e
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-10-26 20:15:44 +0900

    fix :: 이미지 확장자 수정

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,7 +1,7 @@
-import beadal_icon from './beadal_icon.jpg'
+import beadal_icon from './beadal_icon.png'
 import search_icon from './search_icon.png'
 import bag_icon from './bag_icon.png'
-import Header from './Header.jpg'
+import Header from './Header.png'
 
 export const assets = {
     beadal_icon,
