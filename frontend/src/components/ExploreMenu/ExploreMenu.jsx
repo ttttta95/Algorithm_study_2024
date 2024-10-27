e60b3e921b5e9a1f8902dfdcbe37e91e52277c8a
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-10-27 10:12:51 +0900

    feat :: Explore Menu

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -6,17 +6,17 @@ const ExploreMenu = () => {
   return (
     <div className='Explore-menu' id='Explore-menu'>
       <h1>Explore our menu</h1>
-      <p className='Explore-menu-text'>Choose from a diverse menu featuring a delectable array of dishes crafted with the finest ingredients and culinary expertise. Our mission is to satisfy your cravings and elevate your dining experience, one delicious meal at a time.</p>
+      <p className='Explore-menu-text'>Choose from a diverse menu featuring a delectable array at dishes. Our mission's to satisfy your cravings and elevate your dining experience.</p>
       <div className="Explore-menu-list">
         {menu_list.map((item, index) => {
           return (
             <div key={index} className='Explore-menu-list-item'>
               <img src={item.menu_image} alt="" />
-              <p>{item.menu_name}</p>
             </div>
           )
         })}
       </div>
+      <hr />
     </div>
   )
 }
