a55fef0984f8259c1f0cfd13d8ab2aa24f297383
윤효진 <163402670+ttttta95@users.noreply.github.com>
2024-10-28 17:55:28 +0900

    remove

--- green-marcket/src/assets/Assets.js
+++
@@ -1,9 +0,0 @@
-import logo from './logo.png'
-import search_icon from './search_icon.png'
-import bag_icon from './bag_icon.png'
-
-export const assets = {
-    logo,
-    search_icon,
-    bag_icon,
-}
